5f5f4f7463f9de6a29747c2106dee780c5442a5a
paul-ace474 <ajangpaul8@gmail.com>
2024-11-13 10:46:47 -0500

    cors added

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connect = require("./config/config");
 const router = require("./router/user.route.js");
+const cors = require("cors")
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors())
 app.use("/api", router);  
 
 connect();
